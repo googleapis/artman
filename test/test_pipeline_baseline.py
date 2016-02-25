233a21d7e6c1e651ccba47fa2696d214c8fe1c82
Jun Mukai <mukai@google.com>
2016-02-25 15:42:26 -0800

    Use tox's tmpdir.
    tempfile module usually uses /tmp/... files, which isn't great
    when testing with tox. Actually, pytest has the feature to supply
    tmpdir as an argument: see https://pytest.org/latest/tmpdir.html
    and http://tox.readthedocs.org/en/latest/example/pytest.html
    
    To provide tmpdir as an argument, the test cases should be defined
    in the way pytest assumes, i.e. not methods of a class under
    unittest2.TestCase but functions in the module with test_ prefix.
    
    Change-Id: Iaff0d81978c0db22367e9f58fd12cad55de4b9ea

--- test/test_pipeline_baseline.py
+++ test/test_pipeline_baseline.py
@@ -1,7 +1,5 @@
 import mock
 import os
-import tempfile
-import unittest2
 
 from pipeline.pipelines import pipeline_factory
 from taskflow import engines
@@ -24,52 +22,53 @@ def get_expected_calls(baseline, binding):
     return commands
 
 
-class TestPipelineBaseline(unittest2.TestCase):
+@mock.patch('subprocess.call')
+def _test_python_baseline(task_name, test_name, tmpdir, mock_call):
 
-    @mock.patch('subprocess.call')
-    def _test_python_baseline(self, task_name, test_name, mock_call):
+    # The real output location of running the pipeline
+    output_dir = str(tmpdir.mkdir(test_name))
 
-        # The real output location of running the pipeline
-        output_dir = tempfile.mkdtemp(suffix=test_name)
+    # Pipeline kwargs
+    kwargs_ = {
+        'src_proto_path':
+            ['test/testdata/gapi-example-library-proto/src/main/proto'],
+        'import_proto_path':
+            ['test/fake-repos/gapi-core-proto/src/main/proto/'],
+        'gapi_tools_path': 'test/fake-repos/gapi-tools',
+        'service_yaml': [
+            'test/testdata/gapi-example-library-proto/src/main/proto/'
+            'google/example/library/library.yaml'],
+        'veneer_yaml': [
+            'test/testdata/gapi-example-library-proto/src/main/proto/'
+                'google/example/library/library_veneer.yaml',
+            'test/testdata/gapi-example-library-proto/src/main/proto/'
+                'google/example/library/python_veneer.yaml'],
+        'output_dir': output_dir,
+        'api_name': 'library-v1',
+        'vgen_output_dir': output_dir}
 
-        # Pipeline kwargs
-        kwargs_ = {
-            'src_proto_path':
-                ['test/testdata/gapi-example-library-proto/src/main/proto'],
-            'import_proto_path':
-                ['test/fake-repos/gapi-core-proto/src/main/proto/'],
-            'gapi_tools_path': 'test/fake-repos/gapi-tools',
-            'service_yaml': [
-                'test/testdata/gapi-example-library-proto/src/main/proto/'
-                'google/example/library/library.yaml'],
-            'veneer_yaml': [
-                'test/testdata/gapi-example-library-proto/src/main/proto/'
-                    'google/example/library/library_veneer.yaml',
-                'test/testdata/gapi-example-library-proto/src/main/proto/'
-                    'google/example/library/python_veneer.yaml'],
-            'output_dir': output_dir,
-            'api_name': 'library-v1',
-            'vgen_output_dir': output_dir}
+    # Create an empty 'fake_output_api.py' in the output_dir. Do not invoke
+    #  'touch' command with subprocess.call() because it's mocked.
+    with open(os.path.join(output_dir, 'fake_output_api.py'), 'w'):
+        pass
 
-        # Create an empty 'fake_output_api.py' in the output_dir. Do not invoke
-        #  'touch' command with subprocess.call() because it's mocked.
-        with open(os.path.join(output_dir, 'fake_output_api.py'), 'w'):
-            pass
+    # Run pipeline
+    pipeline = pipeline_factory.make_pipeline(task_name, **kwargs_)
+    engine = engines.load(pipeline.flow, engine='serial')
+    engine.run()
 
-        # Run pipeline
-        pipeline = pipeline_factory.make_pipeline(
-            task_name, **kwargs_)
-        engine = engines.load(pipeline.flow, engine='serial')
-        engine.run()
+    # Compare with the expected subprocess calls.
+    mock_call.assert_has_calls(get_expected_calls(
+        test_name, {'CWD': os.getcwd(), 'OUTPUT': output_dir}))
 
-        # Compare with the expected subprocess calls.
-        mock_call.assert_has_calls(get_expected_calls(
-            test_name, {'CWD': os.getcwd(), 'OUTPUT': output_dir}))
 
-    def test_python_grpc_client_baseline(self):
-        self._test_python_baseline('PythonGrpcClientPipeline',
-                                   'python_grpc_client_pipeline')
+def test_python_grpc_client_baseline(tmpdir):
+    _test_python_baseline('PythonGrpcClientPipeline',
+                          'python_grpc_client_pipeline',
+                          tmpdir)
 
-    def test_python_vkit_client_baseline(self):
-        self._test_python_baseline('PythonVkitClientPipeline',
-                                   'python_vkit_client_pipeline')
+
+def test_python_vkit_client_baseline(tmpdir):
+    _test_python_baseline('PythonVkitClientPipeline',
+                          'python_vkit_client_pipeline',
+                          tmpdir)
