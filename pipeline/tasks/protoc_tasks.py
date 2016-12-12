753c96e98ec0f0d3673ee7153d31c80ddce43aad
Jacob Geiger <jgeiger@google.com>
2016-12-12 09:13:04 -0800

    Change Grpc MetdataGen args (#125)
    * Change Grpc MetdataGen args
    
    Corresponds to https://github.com/googleapis/toolkit/pull/851, which
    removes the `--name` parameter.

--- pipeline/tasks/protoc_tasks.py
+++ pipeline/tasks/protoc_tasks.py
@@ -475,11 +475,6 @@ class GrpcPackageMetadataGenTask(task_base.TaskBase):
         packman_api_name_parts = packman_name.split('/')
         packman_api_name = '/'.join(packman_api_name_parts[:-1])
 
-        # Currently corresponds to packman's `titlename`. To be replaced
-        # by data from service config; see googleapis/toolkit#270
-        packman_title_name = ('Google ' + api_name[0].upper() +
-                              api_name[1:])
-
         args = [
             '--descriptor_set=' + os.path.abspath(descriptor_set),
             '--input=' + os.path.abspath(intermediate_package_dir),
@@ -489,7 +484,6 @@ class GrpcPackageMetadataGenTask(task_base.TaskBase):
             '--defaults_config=' + os.path.abspath(package_defaults_yaml),
             '--language=' + language,
             '--short_name=' + api_name,
-            '--name=' + packman_title_name,
             '--googleapis_path=' + packman_api_name,
             '--version=' + api_version
         ] + service_args
