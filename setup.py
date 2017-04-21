8d41c74901483445023481c31e0160e9a6accb94
Luke Sneeringer <luke@sneeringer.com>
2017-04-21 10:05:58 -0700

    Re-add the smoke test in the Dockerfile. (#165)

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with io.open('%s/requirements.txt' % cur_dir) as requirements_file:
 
 setuptools.setup(
     name='googleapis-artman',
-    version='0.2.1',
+    version='0.3.0',
     description='Google API artifact manager',
     author='Google Inc',
     author_email='googleapis-packages@google.com',
