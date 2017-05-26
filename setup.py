7271f0e8282245eeec0c1b946d97ebd47064c14a
Luke Sneeringer <luke@sneeringer.com>
2017-05-26 09:44:53 -0700

    Fix dependency issues. (#200)

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with io.open('%s/requirements.txt' % cur_dir) as requirements_file:
 
 setuptools.setup(
     name='googleapis-artman',
-    version='0.4.3',
+    version='0.4.4',
     description='Google API artifact manager',
     author='Google Inc',
     author_email='googleapis-packages@google.com',
