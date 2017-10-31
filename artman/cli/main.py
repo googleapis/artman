fce69f780875ebeb8bd5043273416aaeaf39da27
Jacob Geiger <jgeiger@google.com>
2017-10-31 14:17:51 -0700

    Update docker image (#313)
    Previous image incompatible with `config_schema_version` in GAPIC yaml

--- artman/cli/main.py
+++ artman/cli/main.py
@@ -40,7 +40,7 @@ from artman.pipelines import pipeline_factory
 from artman.utils import config_util
 from artman.utils.logger import logger, setup_logging
 
-ARTMAN_DOCKER_IMAGE = 'googleapis/artman:0.5.0'
+ARTMAN_DOCKER_IMAGE = 'googleapis/artman:0.5.0-rc02'
 RUNNING_IN_ARTMAN_DOCKER_TOKEN = 'RUNNING_IN_ARTMAN_DOCKER'
 
 
