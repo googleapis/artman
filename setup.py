f7d8ba399f698badd83414bd76f0b18775692ef9
Andrea Lin <andrealin@google.com>
2018-07-26 18:08:03 -0700

    Release artman 0.14.3 (#482)
    * release artman v0.14.3
    
    * release artman v0.14.3
    
    * release artman v0.14.3
    
    * also update setup.py with 0.14.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ import setuptools
 
 # The following line is parsed by CI scripts (see .circleci/config.yml)
 # and by release.py. Please keep the format.
-current_version = '0.14.2'
+current_version = '0.14.3'
 
 cur_dir = os.path.realpath(os.path.dirname(__file__))
 with io.open('%s/requirements.txt' % cur_dir) as requirements_file:
