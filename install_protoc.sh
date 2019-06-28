90ab99c50301e988d469eceb98c6978f9f7bc548
Alexander Fenster <fenster@google.com>
2019-06-27 17:47:08 -0700

    use protoc v3.9.0-rc1 for PHP (#655)
    * use protoc v3.9.0-rc1 for PHP
    
    Ads ask to try using `protoc` v3.9.0-rc1 for PHP (download location is [here](https://github.com/protocolbuffers/protobuf/releases/download/v3.9.0-rc1/protoc-3.9.0-rc-1-linux-x86_64.zip)). Internal bug ID: 131809012.
    
    * some URLs are weird

--- install_protoc.sh
+++ install_protoc.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/env bash
 
+### TODO(alexander-fenster, vam-google): 
+### The whole idea of this script is wrong. 
+### We either should not use different protoc versions across languages,
+### or should invent a better way of managing this kind of dependency. 
+
 # Store protobuf versions in an associative array keyed by language.
 declare -A protobuf_versions
+declare -A override_download_location
 
 # Please adhere to this format, as artman parses these lines for the protobuf versions.
 protobuf_versions[nodejs]=3.8.0
 protobuf_versions[go]=3.8.0
 protobuf_versions[python]=3.8.0
 protobuf_versions[ruby]=3.8.0
-protobuf_versions[php]=3.8.0
+protobuf_versions[php]=3.9.0-rc1
 protobuf_versions[csharp]=3.8.0
 # Protobuf Java dependency must match grpc-java's protobuf dep.
 protobuf_versions[java]=3.7.1
 
+# RC1 url has no logic: compare rc1 in the folder name with rc-1 in the filename
+override_download_location[3.9.0-rc1]=https://github.com/protocolbuffers/protobuf/releases/download/v3.9.0-rc1/protoc-3.9.0-rc-1-linux-x86_64.zip
 
 # Install each unique protobuf version.
 for i in "${protobuf_versions[@]}"
 do
+  location=https://github.com/google/protobuf/releases/download/v${i}/protoc-${i}-linux-x86_64.zip
+  if [ "${override_download_location[$i]}" != "" ]; then
+    location=${override_download_location[$i]}
+  fi
   mkdir -p /usr/src/protoc-${i}/ \
-      && curl --location https://github.com/google/protobuf/releases/download/v${i}/protoc-${i}-linux-x86_64.zip > /usr/src/protoc-${i}/protoc.zip \
+      && curl --location $location > /usr/src/protoc-${i}/protoc.zip \
       && cd /usr/src/protoc-${i}/ \
       && unzip protoc.zip \
       && rm protoc.zip \
